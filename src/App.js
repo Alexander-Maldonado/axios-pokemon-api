0c7b4626e3ac3403a83592632bdf103548064a1f
Alexander-Maldonado <alex_ahc@yahoo.com>
2022-07-04 00:09:43 -0700

    pokemon apit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+    .then((response) => {
+      console.log(response.data.results);
+      setPokemons(response.data.results)
+    })
+    .catch((err)=>console.log(err));
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='pokemonTitle'>Pokemon List</h1>
+      <ul>
+        {pokemons.map((pokemon, index)=>(
+          <li className='pokeList' key={index}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
